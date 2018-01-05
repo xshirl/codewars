85ce37a46c77e63e10bb9ac7d4321b8d490e00c2
Shirley <xshirl525@gmail.com>
2018-01-05 09:46:43 -0500

    Update arethenumbersinorder.js

--- arethenumbersinorder.js
+++ arethenumbersinorder.js
@@ -1,20 +1,8 @@
 //In this Kata, your function receives an array of positive integers as input. 
 //Your task is to determine whether the numbers are in ascending order.
 
-function inAscOrder(arr) {
-  var num = 0;
-  for (var i = 0; i<arr.length; i++) {
-    if (arr[i] >= num) {
-    num = arr[i];
-    }
-    else {
-    num = arr[i-1];
-    }
-    }
-  if (num == arr[arr.length-1]) {
-    return true;
-  }
-  else {
-    return false;
-  }
+function isAscending(arr) {
+    return arr.every(function (x, i) {
+        return i === 0 || x >= arr[i - 1];
+    });
 }
